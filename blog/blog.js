0ac15f15b336e8f1ecea4c0c8bc652fa52576ccc
lmccausland54 <ln.mccausland@gmail.com>
2024-10-24 18:00:25 -0600

    week06 update

--- blog/blog.js
+++ blog/blog.js
@@ -1,27 +1,59 @@
+// Reference to the container where articles will be inserted
+const articlesContainer = document.querySelector('#maincontent');
+
+// Example data for the articles
 const articles = [
-	{
-		id: 1,
-		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
-		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
-		imgAlt: 'Book cover for Septimus Heap 1',
-		ages: '10-14',
-		genre: 'Fantasy',
-		stars: '****'
-	},
-	{
-		id: 2,
-		title: 'Magnus Chase Book One: Sword of Summer',
-		date: 'December 12, 2021',
-		description:
-			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
-		imgSrc:
-			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
-		imgAlt: 'Book cover for Magnus Chase 1',
-		ages: '12-16',
-		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
-	}
-]
+  {
+    date: 'July 5, 2022',
+    ageRange: '10-14',
+    genre: 'Fantasy',
+    rating: '★★★★☆',
+    title: 'Septimus Heap Book One: Magyk',
+    image: 'magyk-img.jpg',
+    description: 'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.'
+  },
+  {
+    date: 'December 12, 2021',
+    ageRange: '12-16',
+    genre: 'Fantasy',
+    rating: '★★★★☆',
+    title: 'Magnus Chase Book One: Sword of Summer',
+    image: 'magnus-chase-img.jpg',
+    description: 'Rick Riordan’s Norse Mythology adventure, after his previous success with Greek, Roman, and Egyptian mythology.'
+  }
+  
+];
+
+// Function to build the HTML for a single article
+function buildArticleHTML(article) {
+  return `
+    <section class="article">
+      <div class="book-info">
+         <h2>${article.date}</h2>
+         <p>${article.ageRange}</p>
+         <p>${article.genre}</p>
+         <p>${article.rating}</p>
+      </div>
+      <div class="book-details">
+         <h2 class="book-title">${article.title}</h2>
+         <img class="img" src="${article.image}" alt="Cover of '${article.title}'">
+         <p>${article.description}</p>
+      </div>
+    </section>`;
+   
+}
+
+// Function to display all articles
+function displayArticles() {
+  // Clear the container before adding new articles
+  articles.forEach(article => {
+    // Build the HTML for each article
+    const articleHTML = buildArticleHTML(article);
+    
+    // Append the article HTML to the container
+    articlesContainer.innerHTML += articleHTML;
+  });
+}
+
+// Call the function to display the articles
+displayArticles();
